7f39f1ca3792d33b3dc1fb6d5b2f53b4d1978635
YUllllllllll <wp201700038@163.com>
2024-01-23 17:31:59 +0800

    增加字体图标

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@root')->name('root');
-Route::get('/_debugbar/assets/stylesheets', [
-    'as' => 'debugbar-css',
-    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@css'
-]);
-
-Route::get('/_debugbar/assets/javascript', [
-    'as' => 'debugbar-js',
-    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@js'
-]);
+//Route::get('/_debugbar/assets/stylesheets', [
+//    'as' => 'debugbar-css',
+//    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@css'
+//]);
+//
+//Route::get('/_debugbar/assets/javascript', [
+//    'as' => 'debugbar-js',
+//    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@js'
+//]);
